060c545cfde1d819089f1327e1d765f36c23a1c1
SkippUser <84076538+SkippUser@users.noreply.github.com>
2022-07-02 23:49:08 +0300

    Add pip

--- create_dist.py
+++ create_dist.py
@@ -1,8 +1,11 @@
 import os
+import sys
+
+pip = 'sudo python3.9 -m pip' if 1 == len(sys.argv) else sys.argv[1] 
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 for req in required:
     print(f'{req}:')
-    os.system('sudo python3.9 -m pip download --no-deps -d dist ' + req)
+    os.system(f'{pip} download --no-deps -d dist ' + req)
