603f5fa8575667015ed7e8ba29800cdcfcbdb4f2
kamnsv <kamnev111@mail.ru>
2022-07-03 18:19:21 +0300

    Change install

--- install.py
+++ install.py
@@ -3,13 +3,21 @@ if '__main__' == __name__:
     import sys
     import os
     
-    py = '/usr/local/bin/python3.9' if 1 == len(sys.argv) else sys.argv[1]
+    py = '/usr/local/bin/python3' if 1 == len(sys.argv) else sys.argv[1]
     exe = '/usr/bin/ziem'
     path = os.getcwd() + '/src'
+    pip = f'{py} -m pip'
     
     print('\n[+] Intall  requirements\n----------------')
-    os.system(f'{py} -m pip install --no-index --find-links dist -r requirements.txt')
+    #os.system(f'{pip} install --no-index --find-links dist -r requirements.txt')
+    
+    with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
+    for req in required:
+        print(f'{req}:')
+        os.system(f'{pip} install {req}')
+    
     print('\n[+] Creat ZIEM link to /bin\n----------------')
     with open(exe, 'w') as f:
         f.write(f'''#!/bin/bash
